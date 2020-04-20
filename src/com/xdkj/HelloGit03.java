a3011460d10d734bbed445ec43632048a9013cea
205360988.com <205360988.com>
2020-04-20 10:20:41 +0800

    HelloGit03.java first

---
+++ src/com/xdkj/HelloGit03.java
@@ -0,0 +1,11 @@
+package com.xdkj;
+
+/**
+ * @author mr guo
+ * @create 2020-04-20 10:15
+ */
+public class HelloGit03 {
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+    }
+}
